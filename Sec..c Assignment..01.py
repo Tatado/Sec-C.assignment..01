70bf2c928d6b4e52b992e6aeb2ee721738334f89
Tatado <87714945+Tatado@users.noreply.github.com>
2022-07-13 12:04:12 +0530

    Add files via upload

---
+++ Sec..c Assignment..01.py
@@ -0,0 +1,70 @@
+
+
+print('Hello')
+
+
+
+
+val1 = 2
+val2 = 3
+  
+# using the addition operator
+res = val1 + val2
+print(res)
+
+
+# In[ ]:
+
+
+val1 = 2
+val2 = 3
+  
+# using the multiplication operator
+res = val1 * val2
+print(res)
+
+
+# In[ ]:
+
+
+Three sides of the triangle is a, b and c:  
+a = float(input('Enter first side: '))  
+b = float(input('Enter second side: '))  
+c = float(input('Enter third side: '))  
+  
+# calculate the semi-perimeter  
+s = (a + b + c) / 2  
+  
+# calculate the area  
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
+print('The area of the triangle is %0.2f' %area)   
+
+
+# In[ ]:
+
+
+int main()    
+{    
+int a=10, b=20;      
+printf("Before swap a=%d b=%d",a,b);      
+a=a+b;//a=30 (10+20)    
+b=a-b;//b=10 (30-20)    
+a=a-b;//a=20 (30-10)    
+printf("\nAfter swap a=%d b=%d",a,b);    
+return 0;  
+}   
+
+
+# In[ ]:
+
+
+(/, use, rand(), function, to, generate, the, number)
+    printf (" The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+      
+    printf (" \n The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+    getch();  
+}  
+
+
